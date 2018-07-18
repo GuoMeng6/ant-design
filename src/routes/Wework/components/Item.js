1f81d891239bde9c288268cf188b9d676136b2a3
guomeng <835126160@qq.com>
2018-07-18 15:29:24 +0800

    item

--- src/routes/Wework/components/Item.js
+++ src/routes/Wework/components/Item.js
@@ -1,52 +1,81 @@
 import React, { Component } from 'react';
 import Animated from 'animated/lib/targets/react-dom';
-import Easing from 'animated/lib/Easing';
+
+const defaultStyle = {
+  position: 'absolute',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  alignItems: 'center',
+  textAlign: 'center',
+  backgroundColor: '#ccedeb', // 浅蓝
+  border: '1px solid #00a699',
+};
+
+const animStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  alignItems: 'center',
+  textAlign: 'center',
+  position: 'absolute',
+  top: -1,
+  left: -1,
+  right: -1,
+  bottom: -1,
+  backgroundColor: '#ffdedf', // 浅红
+  border: '1px solid #f34146',
+};
 
 const styles = {
-  peskView: {
-    position: 'relative',
-    display: 'flex',
-    flexDirection: 'row',
-    width: '112px',
-    height: '54px',
-    justifyContent: 'center',
-    alignItems: 'center',
-    textAlign: 'center',
-    borderRadius: 8,
-    backgroundColor: '#ccedeb', //浅蓝
-    border: '1px solid #00a699',
+  beijing: {
+    default: {
+      ...defaultStyle,
+      width: '36px',
+      height: '18px',
+    },
+    small: {
+      ...defaultStyle,
+      width: '36px',
+      height: '18px',
+    },
+    large: {
+      ...defaultStyle,
+      width: '84px',
+      height: '42px',
+    },
+    square: {
+      ...defaultStyle,
+      width: '42px',
+      height: '42px',
+    },
   },
-  peskView2: {
-    position: 'relative',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    textAlign: 'center',
-    borderRadius: 8,
-    position: 'absolute',
-    top: -1,
-    left: -1,
-    right: -1,
-    bottom: -1,
-    backgroundColor: '#ffdedf', //浅红
-    border: '1px solid #f34146',
-  },
-  peskView3: {
-    position: 'relative',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    textAlign: 'center',
-    borderRadius: 8,
-    position: 'absolute',
-    top: -1,
-    left: -1,
-    right: -1,
-    bottom: -1,
-    backgroundColor: '#DCDCDC', //浅灰
-    border: '1px solid #808080',
+  nanjing: {
+    default: {
+      ...defaultStyle,
+      width: '24px',
+      height: '12px',
+    },
+    small: {
+      ...defaultStyle,
+      width: '21px',
+      height: '10px',
+    },
+    large: {
+      ...defaultStyle,
+      width: '24px',
+      height: '12px',
+    },
+    square: {
+      ...defaultStyle,
+      width: '14px',
+      height: '14px',
+    },
+    column: {
+      ...defaultStyle,
+      width: '10px',
+      height: '21px',
+    },
   },
 };
 
@@ -77,17 +106,17 @@ class Item extends Component {
     Animated.timing(this.state.anim, { toValue: 0, duration: 300 }).start();
   }
 
+  getStyle(type) {
+    const arr = type.split(' ');
+    return styles[arr[0]][arr[1]];
+  }
+
   render() {
-    const { data, title, style, hideValue } = this.props;
+    const { type, style } = this.props;
+    const deskType = this.getStyle(type);
     return (
-      <div style={Object.assign({}, styles.peskView, style)}>
-        <Animated.div style={Object.assign({}, styles.peskView2, { opacity: this.state.anim })}>
-          <font style={{ fontSize: 30, color: '#f34146' }}>{data.value}</font>
-        </Animated.div>
-        <font style={{ fontSize: 30, color: '#00a699' }}>{data.value}</font>
-        <Animated.div style={Object.assign({}, styles.peskView3, { opacity: data.status === '离线' || data.value === '' ? 1 : 0 })}>
-          <font style={{ fontSize: 24, color: '#808080' }}>{data.status === '离线' ? '离线' : '空'}</font>
-        </Animated.div>
+      <div style={Object.assign({}, deskType, style)}>
+        <Animated.div style={Object.assign({}, animStyle, { opacity: this.state.anim })} />
       </div>
     );
   }
