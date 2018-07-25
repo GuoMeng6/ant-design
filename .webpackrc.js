ea90dd369ea6594a106db7f79ec05d0b2c0f3d79
shuo.hao <shuo.hao@9-a-m.com>
2018-07-25 18:18:14 +0800

    修改

--- .webpackrc.js
+++ .webpackrc.js
@@ -24,7 +24,7 @@ export default {
     javascriptEnabled: true,
   },
   disableDynamicImport: true,
-  // publicPath: '/',
-  publicPath: 'http://popularize.9-a-m.com/west/',
+  publicPath: '/',
+  // publicPath: 'http://popularize.9-a-m.com/west/',
   hash: true,
 };
